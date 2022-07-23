4633903a7cf1e182c03ad52d3c13fe00620593e0
Akhil Khan <akhilkhan0786khan@gmail.com>
2022-07-24 00:32:41 +0530

    loginContact

--- src/Components/ContactUsPage/Contact.jsx
+++ src/Components/ContactUsPage/Contact.jsx
@@ -1,15 +1,23 @@
-import { Box, Heading, Stack } from "@chakra-ui/react";
+import { Box, Button, Container, Heading, Input, Stack, Text, Textarea } from "@chakra-ui/react";
 import React from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 const Contact = () => {
   return (
-    <Box>
+    <Box bgGradient='linear(to-r, purple.200, gray.500)'>
       <Navbar />
-      <Stack bg="aliceblue" h="400px">
-          <Heading>Contact</Heading>
-      </Stack>
+      <Box textAlign="center"  w="100%" h="auto" mb="2rem">  
+      <Heading onClick={() => alert("Please fill the form")} _hover={{color:"white", cursor:"pointer"}}>Contact Us</Heading>
+      <Container  mt="2rem" rounded="1rem" boxShadow='dark-lg' py="4rem" bg="aliceblue" h="auto">
+          <Input my="1rem" placeholder='Enter your name' size='lg' />
+          <Input  my="1rem" placeholder='Enter your email' size='lg' />
+          <Input  my="1rem" placeholder='Enter your subject' size='lg' />
+          <Textarea placeholder='Message' />
+          <Button my="1rem" w="100%" _hover={{color:"white"}} bg='#9676ff' size='lg'>SEND</Button>
+          <Text color="gray">Can’t remember your password?</Text>
+      </Container>
+      </Box>
       <Footer />
     </Box>
   );
--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -1,15 +1,21 @@
-import { Box, Heading, Stack } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Input, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
 
 const Login = () => {
   return (
-    <Box>
+    <Box bgGradient='linear(to-r, purple.200, gray.500)' >
       <Navbar />
-      <Stack bg="aliceblue" h="400px">
-          <Heading>Login</Heading>
-      </Stack>
+      <Box textAlign="center"  w="100%" h="auto" mb="3rem">  
+      <Heading onClick={() => alert("Please fill the form")} _hover={{color:"white", cursor:"pointer"}} >Log In</Heading>
+      <Container mt="2rem" rounded="1rem" boxShadow='dark-lg' py="4rem" bg="aliceblue" h="auto">
+          <Input my="1rem" placeholder='Enter your email' size='lg' />
+          <Input  my="1rem" placeholder='Enter your password' size='lg' />
+          <Button my="1rem" w="100%" _hover={{color:"white"}} bg='#9676ff' size='lg'>   LOG IN </Button>
+          <Text color="gray">Can’t remember your password?</Text>
+      </Container>
+      </Box>
       <Footer />
     </Box>
   )
